012a1e10409e03565d7ef888fe655b7e1b2285c2
Sergei Anisimov <s.anisimov.it@gmail.com>
2024-09-11 22:20:45 +0200

    Few changes

---
+++ src/Game.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Game.h"
+
+using namespace std;
+
+private:
+    WindowBuilder window;
+
+
+int main() {
+    
+}
---
+++ src/WindowBuilder.cpp
@@ -0,0 +1,7 @@
+#include "WindowBuilder.h"
+
+using namespace std;
+
+CreateWindow(int width, int height, string windowName){
+    InitWindow(width, height, windowName)
+}
